3a6adae871d6d8c83ebb946857434629bb0ae141
Md Tariqul Islam <metariqul91@gmail.com>
2020-03-18 19:57:25 +0600

    revisioned

--- js/custom.js
+++ js/custom.js
@@ -16,8 +16,8 @@ $('.platform-carousel').owlCarousel({
     items: 1,
     loop:true,
     autoplay: true,
-    autoplayTimeout:3000,
-    dots: false
+    dots: false,
+    animateOut: 'fadeOut',
 });
     /* =================================
             ===  Sticky Menu       ===
